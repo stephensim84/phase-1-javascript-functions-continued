d03ca819126bf6383e8c599a2dc56aa7e7ab53a5
Stephen Simmons <stephensimmons@stephens-MacBook-Pro.local>
2022-03-14 08:45:01 -0700

    commit tree 7ed57b3bd7aff524030af530076fc4602fba1d03

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate"){
+
+    console.log(activity);
+
+    return `This Saturday, I want to ${activity}!`
+
+}
+
+function mondayWork(activity = "go to the office"){
+
+    return `This Monday, I will ${activity}.`;
+
+}
+
+function wrapAdjective(asterick = "*") {
+    
+
+ 
+    return function (today = "special") {
+
+    return `You are ${asterick}${today}${asterick}!`;
+
+}
+
+  
+}
